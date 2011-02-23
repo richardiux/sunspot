34ed50652f2dadb0eb26d5d889e7aa458f9f931e
Richard Millan <richardiux@gmail.com>
2011-02-22 23:30:28 -0800

    Making sunspot compatible with kaminari for pagination

--- sunspot/lib/sunspot/search/abstract_search.rb
+++ sunspot/lib/sunspot/search/abstract_search.rb
@@ -248,6 +248,34 @@ module Sunspot
         add_facet(name, DateFacet.new(field, self, options))
       end
   
+      #
+      # Methods used for pagination used by kaminari
+      # 
+  
+      # ==== Returns
+      #
+      # Integer:: Current page number
+      #
+      def current_page
+        @query.page
+      end
+  
+      # ==== Returns
+      #
+      # Integer:: Total number of pages for matching documents
+      #
+      def num_pages
+        (total.to_f / @query.per_page).ceil
+      end
+  
+      # ==== Returns
+      #
+      # Integer:: Number of records displayed per page
+      #
+      def limit_value
+        @query.per_page
+      end
+  
       private
   
       def dsl
